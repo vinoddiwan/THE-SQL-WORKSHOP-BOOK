7ddc8253db2fe2b8412ecd82e2207f99a8319f7b
vinoddiwan <65565944+vinoddiwan@users.noreply.github.com>
2021-02-27 12:19:57 +0530

    Update file.sql

--- Chapter-5 Shaping Data/file.sql
+++ Chapter-5 Shaping Data/file.sql
@@ -2,8 +2,16 @@ SELECT *
 FROM Student
 WHERE course = 'Electronics';
 
+-- The store manager wants a list of all the items that are priced over $14.99 and wants to label them as high-value products
 SELECT 
     ProductName AS 'High-value Products',
     NetRetailPrice
 FROM products
 WHERE NetRetailPrice > 14.99;
+
+-- To include products that have a NetRetailPrice of $14.99 in the previous results
+SELECT 
+    ProductName AS 'High-value Products',
+    NetRetailPrice
+FROM products
+WHERE NetRetailPrice >= 14.99;
