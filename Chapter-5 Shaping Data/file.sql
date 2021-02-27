f0ec250c7330a225a69201ed5299c6c550ad31a3
vinoddiwan <65565944+vinoddiwan@users.noreply.github.com>
2021-02-27 12:10:29 +0530

    Update file.sql

--- Chapter-5 Shaping Data/file.sql
+++ Chapter-5 Shaping Data/file.sql
@@ -3,7 +3,7 @@ FROM Student
 WHERE course = 'Electronics';
 
 SELECT 
-	ProductName AS 'High-value Products',
+    ProductName AS 'High-value Products',
     NetRetailPrice
 FROM products
 WHERE NetRetailPrice > 14.99;
