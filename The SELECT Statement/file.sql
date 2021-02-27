83e169d88685c8ac4d09f40679675989f837a002
vinoddiwan <65565944+vinoddiwan@users.noreply.github.com>
2021-02-27 13:51:35 +0530

    Update file.sql

--- The SELECT Statement/file.sql
+++ The SELECT Statement/file.sql
@@ -1,4 +1,25 @@
 -- Ordering Rows According to Multiple Columns
 SELECT 
-      firstName, CustomerID
-FROM Customers;
+      FirstName, CustomerID
+FROM Customers
+ORDER BY FirstName, CustomerID DESC;
+
+-- Tip for Order By Clause USE integer abbreviation
+SELECT 
+      FirstName, CustomerID
+FROM Customers
+ORDER BY 1, 2;
+
+SELECT
+      FirstName, CustomerID
+FROM Customers
+ORDER BY 1 DESC, 2 DESC;
+
+-- Using LIMIT
+-- The store manager wants to identify the five most expensive items from the product catalog.
+SELECT
+	ProductName, NetRetailPrice
+FROM products
+ORDER BY NetRetailPrice DESC
+LIMIT 5;
+      
