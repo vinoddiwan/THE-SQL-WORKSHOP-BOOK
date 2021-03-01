41412bd2edb734b47331bb212abd10484aefbaaf
vinoddiwan <65565944+vinoddiwan@users.noreply.github.com>
2021-03-01 21:24:42 +0530

    Update file.sql

--- Ch-1 SQL Basics/file.sql
+++ Ch-1 SQL Basics/file.sql
@@ -1,1 +1,13 @@
+-- Create Database
+CREATE database studentdemo;
 
+-- Create Table
+CREATE TABLE student
+(
+StudentId CHAR(4),
+StudentName VARCHAR(30),
+grade CHAR(1),
+age INT,
+course VARCHAR(50),
+PRIMARY KEY(StudentId)
+);
